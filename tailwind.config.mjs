b765357f57d4e060f196cf1f95200b65a4b77bf7
Fredy <afredybustos@gmail.com>
2023-10-29 22:51:50 -0500

    update: tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			gridTemplateColumns: {
+				'post': 'repeat(auto-fill, minmax(min(100%, 22rem), 1fr))'
+			}
+		},
 	},
 	plugins: [
 		require('@tailwindcss/typography')
